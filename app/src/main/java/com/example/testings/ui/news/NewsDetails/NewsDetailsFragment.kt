a78e7bd5958660190c80b72b821f378b9ffdad2e
V1ntagezTV <insur.kunafin@yandex.ru>
2020-02-04 22:49:06 +0500

    NewsDetailsFragment view/content update

--- app/src/main/java/com/example/testings/ui/news/NewsDetails/NewsDetailsFragment.kt
+++ app/src/main/java/com/example/testings/ui/news/NewsDetails/NewsDetailsFragment.kt
@@ -12,17 +12,20 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.w3c.dom.Text
+import java.io.IOException
 
 
 class NewsDetailsFragment: Fragment(){
-    private var ContentText: String = ""
+
     private var ImageLinkArray: ArrayList<String> = ArrayList()
-    private var linkPage: String = ""
+    private lateinit var linkPage: String
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_news_details, container, false)
         linkPage = getLinkOrCloseFragment()
-        getPageContent()
+
+        setPageContent()
 
         return root
     }
@@ -35,34 +38,39 @@ class NewsDetailsFragment: Fragment(){
         return ""
     }
 
-    fun getPageContent(){
+    fun setPageContent(){
         GlobalScope.launch {
-            val doc: Document = Jsoup.connect(linkPage).get()
-            val elements = doc.select("div[class=entry-content clearfix single-post-content]")
-            for (el in elements){
+            try {
+                var ContentText: String = ""
+                var ContentImageUrls = ""
+                val doc: Document = Jsoup.connect(linkPage).get()
+                val content = doc.select("div[class=entry-content clearfix single-post-content]")
+                val header = doc.select("div[class=post-header post-tp-1-header]")
+                val galleryImageList = content
+                    .select("div[id=gallery-2]")
+                    .select("figure[class=gallery-item]")
+
+                for (el in content){
+                    ContentText += el.text() + '\n'+ '\t'
+                }
+
+                for (num in 0 until galleryImageList.size){
+                    val inLink = galleryImageList
+                        .select("div[class=gallery-icon landscape]")
+                        .select("a")
+                        .eq(num)
+                        .attr("href")
+                    ContentImageUrls += inLink + '\n'
+                }
+
                 GlobalScope.launch(Dispatchers.Main) {
-                    val r = el.text()+'\n' + view?.findViewById<TextView>(R.id.urlnewsDetails)?.text
-                    view?.findViewById<TextView>(R.id.urlnewsDetails)?.text = r
+                    view?.findViewById<TextView>(R.id.newsDet_imageurls)?.text = ContentImageUrls
+                    view?.findViewById<TextView>(R.id.newsDet_content)?.text = ContentText
                 }
             }
+            catch (e: IOException){
 
-            val galleryImageList = elements
-                .select("div[id=gallery-2]")
-                .select("figure[class=gallery-item]")
-            //start news images
-            for (num in 0 until galleryImageList.size){
-                val inLink = galleryImageList
-                    .select("div[class=gallery-icon landscape]")
-                    .select("a")
-                    .eq(num)
-                    .attr("href")
-
-                GlobalScope.launch(Dispatchers.Main) {
-                    val r = inLink + '\n' + view?.findViewById<TextView>(R.id.urlImageText)?.text
-                    view?.findViewById<TextView>(R.id.urlImageText)?.text = r
-                }
             }
-            //
         }
     }
 }
