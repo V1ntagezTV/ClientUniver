c42bf2e2810228f8c0a5e425fdfd411aa1f4a0ff
V1ntagezTV <insur.kunafin@yandex.ru>
2020-04-06 11:27:11 +0500

    new package refactoring

--- app/src/main/java/com/example/testings/ui/events/EventDetails/EventDetailsActivity.kt
+++ app/src/main/java/com/example/testings/ui/events/EventDetails/EventDetailsActivity.kt
@@ -1,9 +1,10 @@
-package com.example.testings.ui.events
+package com.example.testings.ui.events.EventDetails
 
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testings.R
+import com.example.testings.ui.events.EventModel
 import com.r0adkll.slidr.Slidr
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
