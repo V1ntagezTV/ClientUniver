f5980afe8b5ad93b740586aa502e85b043dedbd5
V1ntagezTV <insur.kunafin@yandex.ru>
2020-04-25 17:34:16 +0500

    information fragment now in progress

--- app/src/main/java/com/example/testings/ui/information/InformationFragment.kt
+++ app/src/main/java/com/example/testings/ui/information/InformationFragment.kt
@@ -8,14 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.testings.R
 
-class ShareFragment : Fragment() {
-
-    private lateinit var shareViewModel: ShareViewModel
+class InformationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        shareViewModel = ViewModelProviders.of(this).get(ShareViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_application, container, false)
+        val root = inflater.inflate(R.layout.fragment_information, container, false)
 
         return root
     }
