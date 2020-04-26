4721cbcc25a359e58549f736c468592b7e237644
V1ntagezTV <insur.kunafin@yandex.ru>
2020-04-26 19:21:23 +0500

    progress bar and text refactoring

--- app/src/main/java/com/example/testings/ui/events/EventDetails/EventDetailsActivity.kt
+++ app/src/main/java/com/example/testings/ui/events/EventDetails/EventDetailsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.testings.ui.events.EventDetails
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testings.R
@@ -16,18 +19,27 @@ import java.io.IOException
 class EventDetailsActivity : AppCompatActivity(){
 
     private var EventPage = EventModel()
+    private lateinit var retry: Button
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Slidr.attach(this)
         setContentView(R.layout.fragment_events_details)
+        retry = findViewById(R.id.eventDet_retry_connection)
+        progressBar = findViewById(R.id.eventDet_progressBar)
         val linkPage = getLinkOrCloseActivity()
         val toolbar = supportActionBar
         toolbar?.title = "Детали"
         toolbar?.setDisplayHomeAsUpEnabled(true)
         setData(linkPage)
+        retry.setOnClickListener{
+            progressBar.visibility = View.VISIBLE
+            retry.visibility = View.INVISIBLE
+            setData(linkPage)
+        }
     }
-    //set in EventPage !content!
+
     private fun setData(link: String){
         GlobalScope.launch {
             try {
@@ -47,12 +59,17 @@ class EventDetailsActivity : AppCompatActivity(){
                 }
 
                 GlobalScope.launch(Dispatchers.Main) {
+                    progressBar.visibility = View.INVISIBLE
+                    retry.visibility = View.INVISIBLE
                     findViewById<TextView>(R.id.eventDet_title).text = EventPage.Title
                     findViewById<TextView>(R.id.eventDet_content).text = EventPage.FullDescription
                 }
             }
             catch (e: IOException){
-
+                GlobalScope.launch(Dispatchers.Main) {
+                    retry.visibility = View.VISIBLE
+                    progressBar.visibility = View.INVISIBLE
+                }
             }
         }
     }
