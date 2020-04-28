4a62737b568f10944896e7d0331d614cdb447bb7
V1ntagezTV <insur.kunafin@yandex.ru>
2020-04-28 11:30:28 +0500

    facultydetails на новой архитектуре

--- app/src/main/java/com/example/testings/ui/faculties/FacultyDetails/FacultyDetails.kt
+++ app/src/main/java/com/example/testings/ui/faculties/FacultyDetails/FacultyDetails.kt
@@ -1,18 +1,12 @@
 package com.example.testings.ui.faculties.FacultyDetails
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.marginBottom
-import androidx.core.view.marginTop
-import androidx.fragment.app.Fragment
 import com.example.testings.R
+import com.example.testings.ui.faculties.FacultyModel
 import com.r0adkll.slidr.Slidr
-import org.w3c.dom.Text
 
 class FacultyDetails: AppCompatActivity() {
 
@@ -24,16 +18,17 @@ class FacultyDetails: AppCompatActivity() {
         Slidr.attach(this)
         setContentView(R.layout.fragment_faculty_details)
         linLayout = findViewById(R.id.facDet_LinearLayout)
-        addProfiles(FacultyDetailsModel.currentData)
-
+        addProfiles()
     }
 
-    private fun addProfiles(data: ArrayList<String>){
+    private fun addProfiles(){
         findViewById<TextView>(R.id.facDet_title).text = FacultyDetailsModel.currentModel?.Name
         findViewById<TextView>(R.id.facDet_content).text = FacultyDetailsModel.currentModel?.Content
-        for (ind in 0 until data.size){
+        val profiles = FacultyDetailsModel.currentModel!!.Profiles
+        for (ind in 0 until profiles.size){
             val textv = TextView(this)
-            textv.text = data[ind]
+            textv.text = profiles[ind]
+            textv.setPadding(0,10,0,0)
             linLayout.addView(textv)
         }
     }
--- app/src/main/java/com/example/testings/ui/faculties/FacultyModel.kt
+++ app/src/main/java/com/example/testings/ui/faculties/FacultyModel.kt
@@ -3,5 +3,6 @@ package com.example.testings.ui.faculties
 class FacultyModel(
     val Name: String,
     val Imageid: Int,
-    val Content: String
+    val Content: String,
+    val Profiles: ArrayList<String>
 )
