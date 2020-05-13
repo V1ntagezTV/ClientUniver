7d0f069a14e7269ae9eabf8954e51117a94f2670
V1ntagezTV <insur.kunafin@yandex.ru>
2020-05-14 03:04:29 +0500

    webview actionbar #1 and some refactoring

--- app/src/main/java/com/example/testings/webview/WebViewActivity.kt
+++ app/src/main/java/com/example/testings/webview/WebViewActivity.kt
@@ -1,7 +1,8 @@
 package com.example.testings.webview
 
 import android.os.Bundle
-import android.webkit.WebResourceRequest
+import android.view.KeyEvent
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -16,24 +17,43 @@ class WebViewActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.web_view_activity)
+        setSupportActionBar(findViewById(R.id.webview_toolbar))
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_exit)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         link = intent.getStringExtra("link")
 
         webView = findViewById(R.id.testing)
         webView.settings.javaScriptEnabled = true
-        webView.settings.builtInZoomControls = true
         webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(
-                view: WebView?,
-                request: WebResourceRequest?
-            ): Boolean {
-                return false
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                supportActionBar?.title = view?.title
+                supportActionBar?.subtitle = url
+            }
+        }
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                setTitle("Loading...")
+                if (newProgress == 100) {
+                    setTitle(R.string.app_name)
+                }
             }
         }
         webView.loadUrl(link)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return super.onSupportNavigateUp()
+        finish()
+        return true
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+
     }
 }
